4bcb68340a4bf1b9e84831be231c9f299061b69a
skeleten <skele@ymail.com>
2016-12-11 15:11:48 +0100

    skeleton for builtins

---
+++ src/eval/builtins.rs
@@ -0,0 +1,5 @@
+use super::EvalContext;
+
+pub fn register(ctx: &mut EvalContext) {
+    /* TODO */
+}
--- src/eval/mod.rs
+++ src/eval/mod.rs
@@ -1,7 +1,21 @@
 use std::collections::HashMap;
 
+mod builtins;
+
 pub struct EvalContext {
     pub bindings: HashMap<String, InternValue>,
+}
+
+impl EvalContext {
+    pub fn new() -> Self {
+        EvalContext {
+            bindings: HashMap::new(),
+        }
+    }
+
+    pub fn register_builtins(&mut self) {
+        builtins::register(self);
+    }
 }
 
 pub enum InternValue {
